8fe921671a1bba42e832ca0173a3d72206e73c49
vborovic <borovikv.vladimir@gmail.com>
2017-04-05 11:32:40 +0300

    added TempExperiment class

---
+++ src/TempExperiment.java
@@ -0,0 +1,11 @@
+/**
+ * Created by vborovic on 4/3/17.
+ */
+public class TempExperiment {
+    public static void main(String[] args) {
+        System.out.println(new Float(2.1).hashCode());
+        System.out.println(new Float(2.2).hashCode());
+        System.out.println(Math.abs("abracadabrahelloworld".hashCode()));
+        System.out.println(new Integer(1).hashCode());
+    }
+}
