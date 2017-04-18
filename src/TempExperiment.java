1a31e5a24e05363216b30cd12afa49e21e7868ca
vborovic <borovikv.vladimir@gmail.com>
2017-04-18 17:38:06 +0300

    dynamic alg temp experiments

--- src/TempExperiment.java
+++ src/TempExperiment.java
@@ -1,11 +1,48 @@
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.stream.IntStream;
+
 /**
  * Created by vborovic on 4/3/17.
  */
 public class TempExperiment {
+
+    private static int bottomUpCutRod(int[] p, int n) {
+        int[] r = new int[n+1];
+        int[] s = new int[n+1];
+        r[0] = 0;
+        for(int j=1; j <= n; j++) {
+            int q = -1;
+            for(int i = 1; i <= j; i++) {
+                System.out.println(j + " - " + i);
+                if (q < p[i-1] + r[j-i]) {
+                    q = p[i-1] + r[j-i];
+                    s[j] = i;
+                }
+            }
+            r[j] = q;
+        }
+        System.out.println(Arrays.toString(r));
+        System.out.println(Arrays.toString(s));
+        for (int k = n; k > 0; k--) {
+            System.out.println(s[n]);
+            k = k - s[n];
+        }
+        return r[n];
+    }
+
+    private static double cutRod(int[] p, int n) {
+        if (n == 0) return 0;
+        double q = Double.NEGATIVE_INFINITY;
+        for (int i = 1; i <= n; i++) {
+            q = Math.max(q, p[i-1] + cutRod(p, n-i));
+        }
+        return q;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Float(2.1).hashCode());
-        System.out.println(new Float(2.2).hashCode());
-        System.out.println(Math.abs("abracadabrahelloworld".hashCode()));
-        System.out.println(new Integer(1).hashCode());
+        int[] p = {1, 5, 8, 9};
+        System.out.println(bottomUpCutRod(p, 4));
+        System.out.println(cutRod(p, 4));
     }
 }
